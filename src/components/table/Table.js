289f70cee3ec48e75e2b21f1ca262c0da14d808f
zenitovich <zenitovich@gmail.com>
2023-03-15 15:20:43 +0300

    Resizing optimized finish

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,8 @@
 /* eslint-disable class-methods-use-this */
-import { $ } from '../../core/dom';
 import { ExcelComponent } from '../../core/ExcelComponent';
 import createTable from './table.template';
+import resizeHandler from './table.resize';
+import shouldResize from './table.functions';
 
 // eslint-disable-next-line import/prefer-default-export
 export class Table extends ExcelComponent {
@@ -19,37 +20,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      // closest ищет ближайшего родителя по параметрам
-      const $parent = $resizer.closest('[data-type="resizable"]');
-      const coords = $parent.getCoords();
-      const type = $resizer.data.resize;
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-
-      document.onmousemove = (e) => {
-        $resizer.$el.style.opacity = 1;
-        if (type === 'col') {
-          const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
-          $parent.css({
-            width: `${value}px`,
-          });
-          // eslint-disable-next-line no-return-assign, no-param-reassign
-          cells.forEach((el) => el.style.width = `${value}px`);
-        } else if (type === 'row') {
-          const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
-          $parent.css({
-            height: `${value}px`,
-          });
-        }
-      };
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        $resizer.$el.style.opacity = 0;
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
